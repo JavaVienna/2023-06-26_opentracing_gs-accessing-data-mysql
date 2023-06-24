42d5f922ab6b7072d0c0937bd8ada14f073e8ab8
Georg Pirklbauer <georg.pirklbauer@dynatrace.com>
2023-06-24 19:14:03 +0200

    create a resource

--- complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ complete/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -1,5 +1,8 @@
 package com.example.accessingdatamysql;
 
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.sdk.resources.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.OS_NAME;
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
 
 @SpringBootApplication
 public class AccessingDataMysqlApplication {
@@ -29,6 +35,15 @@ public class AccessingDataMysqlApplication {
 
 		// At this point, we have an authorization header and endpoints for all three signals.
 		// Let's start setting up OTel!
+
+		// The OpenTelemetry resource contains general info for all signals (e.g., OS, Java version, service name etc.)
+		Resource resource = Resource.getDefault().merge(
+				Resource.create(
+						Attributes.of(
+								SERVICE_NAME, AccessingDataMysqlApplication.class.getSimpleName(),
+								OS_NAME, "Windows 11",
+								AttributeKey.stringKey("java.version"), System.getProperty("java.version")
+						)));
 
 
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
